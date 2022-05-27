e27fb9d1bec2cf139803c4c0304f8e22045dd97c
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-05-27 19:54:06 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write($"Quantos numeros: ");
+int quantosNumeros = Convert.ToInt32(Console.ReadLine()!);
+int contador = 1;
+while (contador <= quantosNumeros)
+{
+    Console.WriteLine($"Número #{contador,3:n0}");
+    contador++;
+}
+
+
